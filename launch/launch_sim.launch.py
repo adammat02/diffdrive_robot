0b774d3f6bf0dc5cd644e3ff0bb9b97776e13926
Adam <275460@student.pwr.edu.pl>
2026-04-06 12:15:24 +0200

    Config gazebo with robot

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,64 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    package_name = "diffdrive_robot"
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+            )]),
+            launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    world = LaunchConfiguration('world')
+
+    world_args = DeclareLaunchArgument(
+        'world',
+        default_value='empty.sdf',
+        description='World to load'
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+        )]),
+        launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description',
+                   '-name', 'diffdrive_robot',
+                   '-z', '0.1'],
+        output='screen'
+    )
+
+    bridge_parameters = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_parameters}'
+        ]
+    )
+
+    return LaunchDescription([
+        rsp,
+        world_args,
+        gazebo,
+        spawn,
+        ros_gz_bridge
+    ])
